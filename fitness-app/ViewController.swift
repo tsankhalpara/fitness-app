c342bf539d14a32c769977c81638e41dffa6f2b9
Tara Sankhalpara <tsankhalpara@mac.com>
2019-08-07 13:56:30 +0100

    Initial Commit

---
+++ fitness-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fitness-app
+//
+//  Created by Tara Sankhalpara on 07/08/2019.
+//  Copyright © 2019 Tara Sankhalpara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
